711d16337585c83376160d1ec653fe122f9d603e
NigithaRavindran <nigithar@gmail.com>
2020-03-27 18:13:46 +0530

    Add files via upload

---
+++ build/precache-manifest.d4d4689b8b89048a3951855a7bf91f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e0f80d9e8262a39a909a2eb8520d5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d906e2ddf31716ccf6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c4759ace832e8e58bf4",
+    "url": "/static/js/2.4cd49608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cd49608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d906e2ddf31716ccf6",
+    "url": "/static/js/main.dbb8c79b.chunk.js"
+  },
+  {
+    "revision": "db648ffa3176abc095be",
+    "url": "/static/js/runtime-main.48af8756.js"
+  }
+]);
